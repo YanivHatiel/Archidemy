cf5ab201535b2133179baca3f0a530b0f4424323
unknown <yanivhatiel@gmail.com>
2025-01-11 19:55:28 +0000

    Update site content

--- searchindex.en.js
+++ searchindex.en.js
@@ -1,14 +1,38 @@
 var relearn_searchindex = [
   {
+    "breadcrumb": "Archidemy \u003e SCE_Courses Overview \u003e Building Information Modeling (BIM)",
+    "content": "Introduction Welcome to the first week of the BIM course. This week, we will focus on the foundational concepts of Building Information Modeling (BIM), its significance in the architectural design process, and its role in facilitating effective collaboration among project stakeholders.\nVideo Recording Tasks for the Week Watch the lecture video and take notes. Review the provided presentation slides. Complete the assigned task and submit it through the submission form. Resources Autodesk Educational Lisence",
+    "description": "Overview of Week 1 topics and assignments.",
+    "tags": [],
+    "title": "01: Introduction to BIM",
+    "uri": "/courses/bim/1/index.html"
+  },
+  {
+    "breadcrumb": "Archidemy \u003e SCE_Courses Overview \u003e Building Information Modeling (BIM)",
+    "content": "Introduction n this session, we will dive deeper into the fundamental elements of building design: walls, floors, and terrain modeling. These components form the structural and aesthetic foundation of any architectural project, and mastering their creation and manipulation is essential for effective BIM workflows.\nVideo Recording",
+    "description": "Overview of Week 1 topics and assignments.",
+    "tags": [],
+    "title": "02: Walls, Floors, Site",
+    "uri": "/courses/bim/2/index.html"
+  },
+  {
+    "breadcrumb": "Archidemy \u003e SCE_Courses Overview \u003e Building Information Modeling (BIM)",
+    "content": "Introduction n this session, we will dive deeper into the fundamental elements of building design: walls, floors, and terrain modeling. These components form the structural and aesthetic foundation of any architectural project, and mastering their creation and manipulation is essential for effective BIM workflows.\nVideo Recording Recording",
+    "description": "Overview of Week 1 topics and assignments.",
+    "tags": [],
+    "title": "03: Walls, Floors, Site",
+    "uri": "/courses/bim/3/index.html"
+  },
+  {
     "breadcrumb": "",
     "content": "Architools Academy Architools Academy provides structured resources for developing architectural design skills, focusing on parametric methodologies, BIM, and bioclimatic strategies.\nSections: Courses: Comprehensive educational content on Parametric Design, BIM, and Bio-Climate. Architools Tutorials: Detailed guides and workflows for effective use of the Architools plugin.",
     "description": "A platform for architectural design education and mastering parametric tools with Architools.",
     "tags": [],
-    "title": "Architools Academy",
+    "title": "Archidemy",
     "uri": "/index.html"
   },
   {
-    "breadcrumb": "Architools Academy",
+    "breadcrumb": "Archidemy",
     "content": "Architools Plugin Tutorials This section provides detailed documentation and tutorials for the Architools plugin, focusing on its integration into architectural design workflows.\nTopics: Getting Started with Architools Core Features and Functions Advanced Architecture Workflows Supplementary Video Tutorials",
     "description": "Comprehensive guides and workflows for effective use of the Architools plugin.",
     "tags": [],
@@ -16,15 +40,7 @@ var relearn_searchindex = [
     "uri": "/architools-tutorials/index.html"
   },
   {
-    "breadcrumb": "Architools Academy",
-    "content": "",
-    "description": "",
-    "tags": [],
-    "title": "Auths",
-    "uri": "/auth/index.html"
-  },
-  {
-    "breadcrumb": "Architools Academy \u003e Courses Overview",
+    "breadcrumb": "Archidemy \u003e SCE_Courses Overview",
     "content": "Bio-Climate This course focuses on the principles and methodologies of bioclimatic design, emphasizing environmental performance analysis and optimization.\nTopics Covered: Climate-responsive architectural strategies Daylighting, thermal comfort, and airflow analysis Practical use of tools such as Ladybug for performance simulations Course Structure: Includes site analysis and design optimization exercises to apply bioclimatic principles.",
     "description": "Study of environmental design strategies and their integration into architectural projects.",
     "tags": [],
@@ -32,15 +48,15 @@ var relearn_searchindex = [
     "uri": "/courses/bio-climate/index.html"
   },
   {
-    "breadcrumb": "Architools Academy \u003e Courses Overview",
-    "content": "Building Information Modeling (BIM) This course introduces BIM as a systematic approach to building design and documentation, focusing on Revit as a key tool for collaborative workflows and data integration.\nTopics Covered: BIM standards and guidelines Revit essentials for architectural design Coordination and documentation in multi-disciplinary projects Course Structure: Includes practical exercises and project-based learning to reinforce BIM concepts.",
+    "breadcrumb": "Archidemy \u003e SCE_Courses Overview",
+    "content": "Building Information Modeling (BIM) This course introduces BIM as a systematic approach to building design and documentation, focusing on Revit as a key tool for collaborative workflows and data integration.\nCourse Structure: Week 01 Week 02 Week 03 Week 04 Week 05 Week 06 Week 07 Week 08 Week 09 Week 10 Week 11 Week 12 Week 13",
     "description": "In-depth training on BIM concepts and their implementation in architectural practice using Revit.",
     "tags": [],
     "title": "Building Information Modeling (BIM)",
     "uri": "/courses/bim/index.html"
   },
   {
-    "breadcrumb": "Architools Academy",
+    "breadcrumb": "Archidemy",
     "content": "",
     "description": "",
     "tags": [],
@@ -48,15 +64,7 @@ var relearn_searchindex = [
     "uri": "/categories/index.html"
   },
   {
-    "breadcrumb": "Architools Academy",
-    "content": "Architools Academy: Course Index Architools Academy offers in-depth courses tailored to architectural professionals seeking to expand their expertise. Each course is designed to provide a systematic understanding of core concepts and their practical applications in architectural design.\nCourses: Bio-Climate Design Building Information Modeling (BIM) Parametric Design",
-    "description": "A structured overview of courses focusing on Bio-Climate Design, Building Information Modeling (BIM), and Parametric Design.",
-    "tags": [],
-    "title": "Courses Overview",
-    "uri": "/courses/index.html"
-  },
-  {
-    "breadcrumb": "Architools Academy \u003e Architools Plugin Tutorials \u003e Overview",
+    "breadcrumb": "Archidemy \u003e Architools Plugin Tutorials \u003e Overview",
     "content": "Data Management and Visualization These tools help manage project data and present it effectively. They are essential for documentation and communicating design intent.\nExamples: Data extraction and schedules Performance metrics visualization Custom data visualizations",
     "description": "Tools for managing and presenting project data.",
     "tags": [],
@@ -64,7 +72,7 @@ var relearn_searchindex = [
     "uri": "/architools-tutorials/overview/data-management/index.html"
   },
   {
-    "breadcrumb": "Architools Academy \u003e Architools Plugin Tutorials \u003e Overview",
+    "breadcrumb": "Archidemy \u003e Architools Plugin Tutorials \u003e Overview",
     "content": "Design Tools This category covers tools for creating and refining architectural designs. These tools help in exploring parametric options and optimizing concepts.\nExamples: Facade generation Parametric form development Pattern design",
     "description": "Tools for generating and optimizing architectural designs.",
     "tags": [],
@@ -72,7 +80,7 @@ var relearn_searchindex = [
     "uri": "/architools-tutorials/overview/design-tools/index.html"
   },
   {
-    "breadcrumb": "Architools Academy \u003e Architools Plugin Tutorials",
+    "breadcrumb": "Archidemy \u003e Architools Plugin Tutorials",
     "content": "Getting Started This section provides all the necessary steps to begin using Architools.\nTopics Covered: Installation instructions Licensing and activation process Basic configuration User interface overview Follow these steps to ensure a smooth setup.",
     "description": "Installation, licensing, and initial setup for Architools.",
     "tags": [],
@@ -80,7 +88,7 @@ var relearn_searchindex = [
     "uri": "/architools-tutorials/getting-started/index.html"
   },
   {
-    "breadcrumb": "Architools Academy \u003e Architools Plugin Tutorials",
+    "breadcrumb": "Archidemy \u003e Architools Plugin Tutorials",
     "content": "Overview Architools organizes tools into key categories that align with architectural workflows. These categories provide a structured approach to design and project management.\nSubtopics Site Analysis Tools Design Tools Site Modeling Data Management and Visualization",
     "description": "Introduction to the core workflows of Architools.",
     "tags": [],
@@ -88,7 +96,7 @@ var relearn_searchindex = [
     "uri": "/architools-tutorials/overview/index.html"
   },
   {
-    "breadcrumb": "Architools Academy \u003e Courses Overview",
+    "breadcrumb": "Archidemy \u003e SCE_Courses Overview",
     "content": "Parametric Design This course covers the theoretical and practical aspects of parametric design. It emphasizes the use of Grasshopper to develop algorithmic workflows that enhance architectural design processes.\nTopics Covered: Introduction to parametric thinking Fundamentals of Grasshopper scripting Advanced applications in architectural design Course Structure: Includes weekly exercises and a final project to apply parametric techniques in a practical context.",
     "description": "Comprehensive training in parametric design principles and techniques using Grasshopper.",
     "tags": [],
@@ -96,7 +104,15 @@ var relearn_searchindex = [
     "uri": "/courses/parametric-design/index.html"
   },
   {
-    "breadcrumb": "Architools Academy \u003e Architools Plugin Tutorials \u003e Overview",
+    "breadcrumb": "Archidemy",
+    "content": "Archidemy: Course Index Archidemy offers in-depth courses tailored to architectural professionals seeking to expand their expertise. Each course is designed to provide a systematic understanding of core concepts and their practical applications in architectural design.\nCourses: Bio-Climate Design Building Information Modeling (BIM) Parametric Design",
+    "description": "A structured overview of courses focusing on Bio-Climate Design, Building Information Modeling (BIM), and Parametric Design.",
+    "tags": [],
+    "title": "SCE_Courses Overview",
+    "uri": "/courses/index.html"
+  },
+  {
+    "breadcrumb": "Archidemy \u003e Architools Plugin Tutorials \u003e Overview",
     "content": "Site Analysis Tools These tools focus on evaluating site-specific conditions and environmental factors. They provide valuable data for informed design decisions.\nExamples: Wind rose analysis Solar studies Shadow impact simulation",
     "description": "Tools for analyzing environmental and site-specific data.",
     "tags": [],
@@ -104,7 +120,7 @@ var relearn_searchindex = [
     "uri": "/architools-tutorials/overview/site-analysis/index.html"
   },
   {
-    "breadcrumb": "Architools Academy \u003e Architools Plugin Tutorials \u003e Overview",
+    "breadcrumb": "Archidemy \u003e Architools Plugin Tutorials \u003e Overview",
     "content": "Site Modeling This category provides tools for creating terrain and modeling the project’s context. It supports better integration with the surrounding environment.\nExamples: Topography creation Massing studies Contextual building modeling",
     "description": "Tools for terrain and context modeling.",
     "tags": [],
@@ -112,7 +128,7 @@ var relearn_searchindex = [
     "uri": "/architools-tutorials/overview/site-modeling/index.html"
   },
   {
-    "breadcrumb": "Architools Academy",
+    "breadcrumb": "Archidemy",
     "content": "",
     "description": "",
     "tags": [],
